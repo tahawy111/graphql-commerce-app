9521ea60746690e5e30467fe4ce0f7c3752d6e5e
tahawy111 <amertahawy111@gmail.com>
2023-06-07 23:35:05 +0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-// const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 
-// const server = new ApolloServer()
+const server = new ApolloServer()
+
+server.listen().then(({url}) => console.log(`Server is ready at ${url}`))
